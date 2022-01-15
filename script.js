6b583e97bcb8d1e8c169165c611ac78687d889b1
danielcsmg <daniel.csouzamg@gmail.com>
2022-01-15 17:01:01 -0300

    Implementar função que pega informações dos personagens

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const imagemPersonagemUm = document.querySelector('#imagemUm')
-const imagemPersonagemDois = document.querySelector('#imagemDois')
-const imagemPersonagemTres = document.querySelector('#imagemTres')
+const imagemPersonagemUm = document.querySelector('#imagemPersonagemUm');
+const imagemPersonagemDois = document.querySelector('#imagemPersonagemDois');
+const imagemPersonagemTres = document.querySelector('#imagemPersonagemTres');
 
-const nomePersonagemUm = document.querySelector('nomePersonagemUm')
-const nomePersonagemDois = document.querySelector('nomePersonagemDois')
-const nomePersonagemTres = document.querySelector('nomePersonagemTres')
+const nomePersonagemUm = document.querySelector('nomePersonagemUm');
+const nomePersonagemDois = document.querySelector('nomePersonagemDois');
+const nomePersonagemTres = document.querySelector('nomePersonagemTres');
 
-const EspeciePersonagemUm = document.querySelector('EspeciePersonagemUm')
-const EspeciePersonagemDois = document.querySelector('EspeciePersonagemDois')
-const EspeciePersonagemTres = document.querySelector('EspeciePersonagemTres')
+const EspeciePersonagemUm = document.querySelector('EspeciePersonagemUm');
+const EspeciePersonagemDois = document.querySelector('EspeciePersonagemDois');
+const EspeciePersonagemTres = document.querySelector('EspeciePersonagemTres');
 
-const condicaoPersonagemUm = document.querySelector('condicaoPersonagemUm')
-const condicaoPersonagemDois = document.querySelector('condicaoPersonagemDois')
-const condicaoPersonagemTres = document.querySelector('condicaoPersonagemTres')
+const condicaoPersonagemUm = document.querySelector('condicaoPersonagemUm');
+const condicaoPersonagemDois = document.querySelector('condicaoPersonagemDois');
+const condicaoPersonagemTres = document.querySelector('condicaoPersonagemTres');
 
-const botao = document.querySelector('#botao')
+const botao = document.querySelector('button');
 
+pegarPersonagens = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/67,3,2`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        const dadosDosPersonagens = data;
+        imagemPersonagemUm.src = dadosDosPersonagens[0].image;
+        imagemPersonagemDois.src = dadosDosPersonagens[1].image;
+        imagemPersonagemTres.src = dadosDosPersonagens[2].image;
+    });
+}
 
+botao.onclick = pegarPersonagens
