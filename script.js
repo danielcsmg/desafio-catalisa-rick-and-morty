c28645e39ba9b75d49a7b6295fc9e635e7470598
danielcsmg <daniel.csouzamg@gmail.com>
2022-01-15 16:24:18 -0300

    Criar as constantes para pegar informações dos personagens

---
+++ script.js
@@ -0,0 +1,19 @@
+const imagemPersonagemUm = document.querySelector('#imagemUm')
+const imagemPersonagemDois = document.querySelector('#imagemDois')
+const imagemPersonagemTres = document.querySelector('#imagemTres')
+
+const nomePersonagemUm = document.querySelector('nomePersonagemUm')
+const nomePersonagemDois = document.querySelector('nomePersonagemDois')
+const nomePersonagemTres = document.querySelector('nomePersonagemTres')
+
+const EspeciePersonagemUm = document.querySelector('EspeciePersonagemUm')
+const EspeciePersonagemDois = document.querySelector('EspeciePersonagemDois')
+const EspeciePersonagemTres = document.querySelector('EspeciePersonagemTres')
+
+const condicaoPersonagemUm = document.querySelector('condicaoPersonagemUm')
+const condicaoPersonagemDois = document.querySelector('condicaoPersonagemDois')
+const condicaoPersonagemTres = document.querySelector('condicaoPersonagemTres')
+
+const botao = document.querySelector('#botao')
+
+
